ed74c5eb36de91febdccf71a0b4adca411632299
get <cs5129606@gmail.com>
2019-02-07 00:09:26 -0700

    update few place

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: 'Shijie Zhou',
-    description: 'Introducing how to setup vuepress project',
+    description: 'Teaching, Learning, and Absorbing',
     locales: {
         // The key is the path for the locale to be nested under.
         // As a special case, the default locale can use '/' as its path.
